43bdf2d777a0a182095c2181127be92bce8e5459
Seth Back <seth@delvecore.com>
2021-01-20 09:57:07 -0800

    bugfix (#29)
    Co-authored-by: Seth Back <sethjback@gmail.com>

--- action.js
+++ action.js
@@ -28,7 +28,7 @@ async function parseTemplate () {
     throw new Error('no files provided')
   }
 
-  consulFiles.forEach(async (cf, index) => {
+  await consulFiles.forEach(async (cf, index) => {
     try {
       await fs.stat(cf.filePath)
       cf.outFile = `${cf.filePath}.parsed`
@@ -46,7 +46,7 @@ async function parseTemplate () {
   if (vaultSecrets) {
     vaultFiles = await JSON.parse(vaultSecrets)
 
-    vaultFiles.forEach(async (vf, index) => {
+    await vaultFiles.forEach(async (vf, index) => {
       try {
         await fs.stat(vf.filePath)
         vaultFiles[index].outFile = `${vf.filePath}.parsed`
@@ -74,7 +74,7 @@ async function parseTemplate () {
     promisify: true
   });
 
-  consulFiles.forEach(async (cf, index) => {
+  await consulFiles.forEach(async (cf, index) => {
     console.log('getting values for the consul file', cf)
     if (!cf.consulKeys) {
       return cf
@@ -132,7 +132,7 @@ async function parseTemplate () {
       endpoint: `${vaultSecure ? 'https://' : 'http://'}${vaultUrl}:${vaultport}`
     });
 
-    vaultFiles.forEach(async (vf, index) => {
+    await vaultFiles.forEach(async (vf, index) => {
       try {
         const vals = await loadVaultValues({ vault, paths: vf.vaultSecrets })
         console.log('got vault vals', vals)
@@ -181,7 +181,7 @@ async function parseTemplate () {
 
   // preParse the files if necessary
   console.log('pre-parsing templates with provided values')
-  consulFiles.forEach((consulFile, index) => {
+  await consulFiles.forEach(async (consulFile, index) => {
     if (consulFile.preParse) {
       console.log(`${consulFile.filePath}`)
       try {
@@ -194,7 +194,7 @@ async function parseTemplate () {
   })
 
   if (vaultFiles) {
-    vaultFiles.forEach((vaultFile, index) => {
+    await vaultFiles.forEach(async (vaultFile, index) => {
       console.log(`${vaultFile.filePath}`)
       if (vaultFile.preParse) {
         try {
@@ -211,7 +211,7 @@ async function parseTemplate () {
   if (vaultFiles) {
     console.log('building secrets data')
     try {
-      vaultFiles.forEach(async (vaultFile, index) => {
+      await vaultFiles.forEach(async (vaultFile, index) => {
         console.log(vaultFile.filePath)
         const secretYaml = await yaml.safeLoad(vaultFile.fileData)
         secretYaml.data = vaultFile.vaultValues
@@ -227,7 +227,7 @@ async function parseTemplate () {
   // Build the consul files
   console.log('building consul data')
   try {
-    consulFiles.forEach(async (consulFile, index) => {
+    await consulFiles.forEach(async (consulFile, index) => {
       // if we didn't grab any values from consul continue
       if (!consulFile.consulValues || consulFile.consulValues.length === 0) {
         return
@@ -252,7 +252,7 @@ async function parseTemplate () {
 
       // if they referenced a vault secret, push it here
       const vs = consulFile.vaultSecrets || []
-      vs.forEach(secret => {
+      await vs.forEach(secret => {
         if (!vaultFiles) throw new Error(`invalid vault secret reference ${secret}`)
 
         const vaultFile = vaultFiles.find(vf => vf.filePath === secret)
@@ -286,7 +286,7 @@ async function parseTemplate () {
 
   console.log('Writing output files');
   try {
-    consulFiles.forEach(async consulFile => {
+    await consulFiles.forEach(async consulFile => {
       console.log(consulFile.outFile)
       await fs.writeFile(consulFile.outFile, consulFile.fileData);
     })
@@ -297,7 +297,7 @@ async function parseTemplate () {
 
   if (vaultFiles) {
     try {
-      vaultFiles.forEach(async vaultFile => {
+      await vaultFiles.forEach(async vaultFile => {
         console.log(vaultFile.outFile)
         await fs.writeFile(vaultFile.outFile, vaultFile.fileData);
       })
@@ -312,54 +312,44 @@ async function parseTemplate () {
 
 module.exports = { parseTemplate };
 
-function loadConsulValues ({ consul, paths }) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    const vals = {}
+async function loadConsulValues ({ consul, paths }) {
+  const vals = {}
 
-    await paths.forEach(async path => {
-      console.log(`getting key vaules from consul at path ${path}`)
-      try {
-        const keys = await consul.kv.get({ key: path, recurse: true });
-        console.log('keys', keys)
+  await paths.forEach(async path => {
+    console.log(`getting key vaules from consul at path ${path}`)
+    try {
+      const keys = await consul.kv.get({ key: path, recurse: true });
+      console.log('keys', keys)
 
-        for (const key of keys) {
-          if (key.Key.slice(-1) === '/') {
-            continue;
-          }
-          const keySplit = key.Key.split('/');
-          vals[keySplit[keySplit.length - 1]] = key.Value;
+      for (const key of keys) {
+        if (key.Key.slice(-1) === '/') {
+          continue;
         }
-      } catch (e) {
-        reject(new Error(`"unable to fetch consul value (${e.messages})"`))
+        const keySplit = key.Key.split('/');
+        vals[keySplit[keySplit.length - 1]] = key.Value;
       }
-    })
-
-    resolve({ data: vals })
+    } catch (e) {
+      throw new Error(`"unable to fetch consul value (${e.messages})"`)
+    }
   })
 }
 
-function loadVaultValues ({ vault, paths }) {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise(async (resolve, reject) => {
-    const vals = {}
+async function loadVaultValues ({ vault, paths }) {
+  const vals = {}
 
-    await paths.forEach(async path => {
-      console.log(`getting secret values from vault at path ${path}`)
+  await paths.forEach(async path => {
+    console.log(`getting secret values from vault at path ${path}`)
 
-      try {
-        const keyList = await vault.list(path);
-        console.log('keylist', keyList)
+    try {
+      const keyList = await vault.list(path);
+      console.log('keylist', keyList)
 
-        for (const key of keyList.data.keys) {
-          const keyValue = await vault.read(`${path}/${key}`);
-          vals[key] = Buffer.from(keyValue.data.value).toString('base64');
-        }
-      } catch (e) {
-        reject(new Error(`"unable to fetch vault secret (${e.messages})"`))
+      for (const key of keyList.data.keys) {
+        const keyValue = await vault.read(`${path}/${key}`);
+        vals[key] = Buffer.from(keyValue.data.value).toString('base64');
       }
-    })
-
-    resolve({ data: vals })
+    } catch (e) {
+      throw new Error(`"unable to fetch vault secret (${e.messages})"`)
+    }
   })
 }
